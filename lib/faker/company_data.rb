d5f9f374ce429cba2bd8d787fc1593b1a2727909
William Weckl <william@wolk.com.br>
2015-02-12 10:00:20 -0200

    0.1.0

--- lib/faker/company_data.rb
+++ lib/faker/company_data.rb
@@ -1,131 +1,86 @@
 module Faker
   class CompanyData
     class << self
-      def department
-        department_all.sample
-      end
-      def department_all
-        ["Administrativo",
-         "Publicidade",
-         "Gráfico",
-         "Garagem",
-         "Oficina",
-         "Almoxarifado",
-         "TI",
-         "Pedido",
-         "Compras",
-         "Estoque",
-         "Vendas",
-         "Importação",
-         "Controle bancário",
-         "Patrimônio",
-         "Ordem de serviço",
-         "BI",
-         "Locação",
-         "Faturamento",
-         "Nota fiscal",
-         "Orçamentos",
-         "Logística",
-         "Legal",
-         "Assessoria",
-         "Jurídico",
-         "Jurídico-legal",
-         "Contas a pagar",
-         "Contas a receber",
-         "Qualidade e Inovação",
-         "Direção Financeira",
-         "Direção",
-         "Financeiro",
-         "Administrativo",
-         "Secretaria-Geral",
-         "Secretaria",
-         "Geral",
-         "Recursos Humanos",
-         "Informática",
-         "Coordenação",
-         "Infraestrutura",
-         "Departamento Pessoal",
-         "Desenvolvimento",
-         "Produção",
-         "Design",
-         "Centro de processamento de dados",
-         "Recepção Geral",
-         "Recepção",
-         "Ouvidoria",
-         "Serviço de atendimento ao consumidor",
-         "Alimentação",
-         "Assessoria Técnica",
-         "Suporte",
-        ]
-      end
-      def team
-        team_all.sample
-      end
-      def team_all
-        ["Geral",
-         "Desenvolvimento",
-         "Desenvolvimento server-side",
-         "Desenvolvimento client-side",
-         "Programação",
-         "Back-end",
-         "Front-end",
-         "Infraestrutura",
-         "Redes",
-         "Informática",
-         "TI",
-         "Tecnologia",
-         "Design",
-         "Design gráfico",
-         "Design de produto",
-         "Artes",
-         "Publicidade",
-         "Contabilidade",
-         "Motoristas",
-         "Mecânicos",
-         "Vendedores",
-         "Consultores",
-         "Consultores BI",
-         "Analistas",
-         "Analistas BI",
-         "Suporte",
-         "SAC",
-         "Cozinheiros",
-         "Recepção",
-         "Gerentes",
-         "Laboratório"
-        ]
-      end
       def shift
         shift_all.sample
       end
       def shift_all
-        [
-            "Geral",
-            "Manhã",
-            "Tarde",
-            "Noite",
-            "Matutino",
-            "Diurno",
-            "Noturno",
-            "Motoristas",
-            "Parcial"
-        ]
+        json_file_hash["shifts"]
       end
-      def group
-        group_all.sample
+      def department
+        department_all.sample
       end
-      def group_all
-        ["Administradores",
-         "Diretores",
-         "Recusos Humanos",
-         "Usuários",
-         "Clientes",
-         "Gestores",
-         "Líderes de equipe",
-         "Estagiários",
-         "Suporte",
-        ]
+      def department_all
+        json_file_hash["departments"].map{ |i| i["name"] }
       end
+      def team(department = nil)
+        team_all(department).sample
+      end
+      def team_all(department = nil)
+        teams_default = json_file_hash["teams"]
+        if department
+          teams = json_file_hash["departments"].select{ |k,v| k["name"] == department }.map{ |i| i["teams"] }.flatten(1).map { |i| i["name"] }
+        else
+          teams = json_file_hash["departments"].map{ |i| i["teams"] }.flatten(1).map { |i| i["name"] }
+        end
+        teams_default.concat(teams).compact
+      end
+      def group(department = nil)
+        group_all(department).sample
+      end
+      def group_all(department = nil)
+        groups_default = json_file_hash["groups"]
+        if department
+          groups = json_file_hash["departments"].select{ |k,v| k["name"] == department }.map{ |i| i["groups"] }.flatten(1)
+        else
+          groups = json_file_hash["departments"].map{ |i| i["groups"] }.flatten(1)
+        end
+        groups_default.concat(groups).compact
+      end
+      def cost_center
+        cost_center_all.sample
+      end
+      def cost_center_all
+        json_file_hash["cost_centers"]
+      end
+      def job_title(department = nil, team = nil)
+        job_title_all(department, team).sample
+      end
+      def job_title_all(department = nil, team = nil)
+        job_titles_default = json_file_hash["job_titles"]
+
+        if department
+          department_hash = json_file_hash["departments"].select{ |k,v| k["name"] == department }
+          job_titles_department_default = department_hash.map{ |i| i["job_titles"] }.flatten(1)
+
+          job_titles_default_with_department_default = job_titles_default.concat(job_titles_department_default).compact
+
+          teams = department_hash.map{ |i| i["teams"] }.flatten(1)
+
+          if team
+            teams_job_titles = teams.select{ |k,v| k["name"] == team }.map{ |i| i["job_titles"] }.flatten(1)
+          else
+            teams_job_titles = teams.map{ |i| i["job_titles"] }.flatten(1)
+          end
+
+          job_titles_default_with_department_default.concat(teams_job_titles).compact
+        else
+          job_titles_departments_default = json_file_hash["departments"].map{ |i| i["job_titles"] }.flatten(1)
+          job_titles_default_with_departments_default = job_titles_default.concat(job_titles_departments_default).compact
+
+          teams = json_file_hash["departments"].map{ |i| i["teams"] }.flatten(1)
+          teams_job_titles = teams.map{ |i| i["job_titles"] }.flatten(1)
+
+          job_titles_default_with_departments_default.concat(teams_job_titles).compact
+        end
+      end
+
+      private
+
+      def json_file_hash
+        JSON.parse(File.read("#{File.expand_path( File.dirname( __FILE__ ))}/company_data.json"))
+      end
+
     end
   end
 end
